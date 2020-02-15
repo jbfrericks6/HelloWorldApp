51bbbc9dd345acc5c5f97d3ec3384f060f3456ae
Jb Frericks <jbf29884@uga.edu>
2020-02-15 11:58:26 -0500

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// TextView textView1 = new TextView("hello");
+        final Button button2;
+        button2 = findViewById(R.id.button1);
+        final TextView tv;
+        tv = findViewById(R.id.textView);
+        tv.setVisibility(View.INVISIBLE);
+        button2.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                if(tv.getVisibility() == View.INVISIBLE) {
+                    tv.setVisibility(View.VISIBLE);
+                }else {
+                    tv.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
