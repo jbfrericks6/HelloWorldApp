0d60f8f6f9641b335c48f0237979df47d33907ee
RonAvi <32112914+RonAvi@users.noreply.github.com>
2020-04-15 13:00:32 -0400

    Update MainActivity.java

--- HelloWorldApp-master/app/src/main/java/com/example/myapplication/MainActivity.java
+++ HelloWorldApp-master/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import java.io.IOException;
 import java.util.UUID;
+import org.w3c.dom.Text;
+
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
     final int REQUEST_PERMISSION_CODE = 1000;
 
+    TextView pitchText;
+    TextView noteText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
+        AudioDispatcher dispatcher =
+                AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e){
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPitch(pitchInHz);
+                    }
+                });
+            }
+        };
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pitchText = (TextView) findViewById(R.id.pitchText);
+        noteText = (TextView) findViewById(R.id.noteText);
 
         if(!checkPermissionFromDevice()) {
             requestPermission();
@@ -116,6 +152,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+    public void processPitch(float pitchInHz) {
+
+        pitchText.setText("" + pitchInHz);
+
+        if(pitchInHz >= 110 && pitchInHz < 123.47) {
+            //A
+            noteText.setText("A");
+        }
+        else if(pitchInHz >= 123.47 && pitchInHz < 130.81) {
+            //B
+            noteText.setText("B");
+        }
+        else if(pitchInHz >= 130.81 && pitchInHz < 146.83) {
+            //C
+            noteText.setText("C");
+        }
+        else if(pitchInHz >= 146.83 && pitchInHz < 164.81) {
+            //D
+            noteText.setText("D");
+        }
+        else if(pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+            //E
+            noteText.setText("E");
+        }
+        else if(pitchInHz >= 174.61 && pitchInHz < 185) {
+            //F
+            noteText.setText("F");
+        }
+        else if(pitchInHz >= 185 && pitchInHz < 196) {
+            //G
+            noteText.setText("G");
+        }
+    }
 
 
     private void setupAudioRecorder() {
